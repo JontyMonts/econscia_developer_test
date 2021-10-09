9d96ad5c7c449cc9af3dfad5b4e0276cd6665847
jonts <jontymonts@gmail.com>
2021-10-09 11:09:12 +0100

    add models to admin

--- econsciaproject/upload_csv/admin.py
+++ econsciaproject/upload_csv/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Building, MeterData, HalfhourlyData
+
+admin.site.register(Building)
+admin.site.register(MeterData)
+admin.site.register(HalfhourlyData)

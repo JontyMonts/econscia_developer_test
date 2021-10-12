5f9a7ac5287807eca869836ee80aa06b068f28ee
jonts <jontymonts@gmail.com>
2021-10-12 21:38:21 +0100

    move views page to root. format csv upload page. polish nav bar

--- econsciaproject/econsciaproject/urls.py
+++ econsciaproject/econsciaproject/urls.py
@@ -7,5 +7,5 @@ from upload_csv import views # do we need this?
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload/', include('upload_csv.urls')),
-    path('view/', include('data_view.urls'))
+    path('', include('data_view.urls'))
 ]
